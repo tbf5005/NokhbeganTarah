904bae632d3d796e23b56e33af0fe6dccaca43c0
tbf5005 <103283645+tbf5005@users.noreply.github.com>
2025-11-30 23:34:23 +0330

    api set

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,10 +6,12 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -21,7 +23,21 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
-    })
+        $middleware->api([
+            HandleCors::class,   // ← ← اضافه کن
+        ]);
+    })->withMiddlewareConfig([
+        HandleCors::class => [
+            'paths' => ['api/*', 'sanctum/csrf-cookie'],
+            'allowed_methods' => ['*'],
+            'allowed_origins' => [
+                'https://designerelite.ir',
+                'https://www.designerelite.com',
+            ],
+            'allowed_headers' => ['*'],
+            'supports_credentials' => true,
+        ],
+    ])
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
